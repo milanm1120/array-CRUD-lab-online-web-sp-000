263e2cc663e2b86206a9d285a6f7c530bad7cca9
Milan Mody <milanm1120@gmail.com>
2021-02-21 05:58:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_end_of_array(array, element)
   array.push("arrays!")
 end
 
-puts add_element_to_end_of_array
+puts add_element_to_end_of_array(array)
 
 # def add_element_to_start_of_array(array, element)
 #   array.unshift("wow")
