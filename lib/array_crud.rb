01302688ed3f005d6fc9cc5da3379a1381a9d2bd
Milan Mody <milanm1120@gmail.com>
2021-02-21 05:56:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["wind", "water", "fire", "earth"]
 end
 
-def add_element_to_end_of_array(array)
+def add_element_to_end_of_array(array, element)
   array.push("arrays!")
 end
 
